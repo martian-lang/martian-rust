b95e89557d960c5274980980e29f02a3c6e509ff
Sreenath Krishnan <sreenathk.89@gmail.com>
2019-08-04 20:29:18 -0700

    Attempt to setup pipeline test in the CI

--- martian/tests/split_test/run_test.sh
+++ martian/tests/split_test/run_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 MROPATH=$PWD
 MROFLAGS="--disable-ui"
-PATH=$(realpath ../../target/release/examples/):$PATH
+PATH=$(realpath ../../../target/release/examples/):$PATH
 mrp pipeline.mro pipeline_test
