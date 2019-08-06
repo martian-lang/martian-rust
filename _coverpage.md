97c3b29502f0af08d2cbea142d85b73b11367950
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-06 00:51:37 +0000

    Deploy martian-lang/martian-rust to github.com/martian-lang/martian-rust.git:gh-pages

---
+++ _coverpage.md
@@ -0,0 +1,16 @@
+<!-- _coverpage.md -->
+
+![logo](media/martian_rust.svg)
+
+# martian-rust <small>1.0.0</small>
+
+> Rust adapter for writing martian pipelines.
+
+* Leverage the strong typing in Rust while writing stage code.
+* Auto-generate mro from Rust stage code.
+* Quickly setup stage boilerplate code.
+
+
+[Martian](https://martian-lang.org/)
+[API Docs](https://sreenathkrishnan.github.io/martian-rust/doc/martian/) <!-- TODO -->
+[Get started](/content/README.md)
---
+++ doc/implementors/core/convert/trait.AsRef.js
@@ -0,0 +1,12 @@
+(function() {var implementors = {};
+implementors["failure"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;dyn <a class=\"trait\" href=\"failure/trait.Fail.html\" title=\"trait failure::Fail\">Fail</a> + 'static&gt; for <a class=\"struct\" href=\"failure/struct.Error.html\" title=\"struct failure::Error\">Error</a>",synthetic:false,types:["failure::error::Error"]},];
+implementors["regex_syntax"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.slice.html\">[</a><a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.u8.html\">u8</a><a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.slice.html\">]</a>&gt; for <a class=\"struct\" href=\"regex_syntax/hir/literal/struct.Literal.html\" title=\"struct regex_syntax::hir::literal::Literal\">Literal</a>",synthetic:false,types:["regex_syntax::hir::literal::Literal"]},];
+implementors["tempdir"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/nightly/std/path/struct.Path.html\" title=\"struct std::path::Path\">Path</a>&gt; for <a class=\"struct\" href=\"tempdir/struct.TempDir.html\" title=\"struct tempdir::TempDir\">TempDir</a>",synthetic:false,types:["tempdir::TempDir"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
