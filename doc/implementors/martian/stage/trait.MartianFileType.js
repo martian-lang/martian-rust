c0b7d7645d631fc45651c7a250614efa0e8ff0dc
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-19 04:36:55 +0000

    Deploy martian-lang/martian-rust to github.com/martian-lang/martian-rust.git:gh-pages

--- doc/implementors/martian/stage/trait.MartianFileType.js
+++ doc/implementors/martian/stage/trait.MartianFileType.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["martian_filetypes"] = [{text:"impl <a class=\"trait\" href=\"martian/stage/trait.MartianFileType.html\" title=\"trait martian::stage::MartianFileType\">MartianFileType</a> for <a class=\"struct\" href=\"martian_filetypes/json_file/struct.JsonFile.html\" title=\"struct martian_filetypes::json_file::JsonFile\">JsonFile</a>",synthetic:false,types:["martian_filetypes::json_file::JsonFile"]},];
+implementors["martian_filetypes"] = [{text:"impl <a class=\"trait\" href=\"martian/stage/trait.MartianFileType.html\" title=\"trait martian::stage::MartianFileType\">MartianFileType</a> for <a class=\"struct\" href=\"martian_filetypes/bin_file/struct.BincodeFile.html\" title=\"struct martian_filetypes::bin_file::BincodeFile\">BincodeFile</a>",synthetic:false,types:["martian_filetypes::bin_file::BincodeFile"]},{text:"impl <a class=\"trait\" href=\"martian/stage/trait.MartianFileType.html\" title=\"trait martian::stage::MartianFileType\">MartianFileType</a> for <a class=\"struct\" href=\"martian_filetypes/json_file/struct.JsonFile.html\" title=\"struct martian_filetypes::json_file::JsonFile\">JsonFile</a>",synthetic:false,types:["martian_filetypes::json_file::JsonFile"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
---
+++ doc/martian_filetypes/bin_file/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["BincodeFile",""],["LazyBincodeReader",""],["LazyBincodeWriter",""]]});
