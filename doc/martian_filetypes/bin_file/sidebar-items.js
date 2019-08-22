4d714aec513a890c28903e68ecc1f9910f3599d1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-22 18:17:39 +0000

    Deploy martian-lang/martian-rust to github.com/martian-lang/martian-rust.git:gh-pages

--- doc/martian_filetypes/bin_file/sidebar-items.js
+++ doc/martian_filetypes/bin_file/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["BincodeFile",""],["LazyBincodeReader",""],["LazyBincodeWriter",""]]});
+initSidebarItems({"struct":[["BincodeFile",""],["LazyBincodeReader","Iterator over individual items  within a bincode file that stores a list of items."],["LazyBincodeWriter","Helper struct to write items one by one to a bincode file that stores a list of items"]]});
