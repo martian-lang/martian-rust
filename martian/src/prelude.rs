f23cbc655e38e4fb12d237ad4a4c6eb88b27be38
Peter Edge <peter.edge@10xgenomics.com>
2021-02-23 19:23:53 -0800

    add UntypedMap to prelude

--- martian/src/prelude.rs
+++ martian/src/prelude.rs
@@ -11,6 +11,7 @@ pub use crate::stage::{
     RawMartianStage, Resource, StageDef,
 };
 pub use crate::{martian_make_mro, MartianAdapter};
+pub use crate::mro::UntypedMap;
 pub use failure::Error;
 pub use log::LevelFilter;
 pub use martian_stages;
