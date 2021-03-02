4ae418cc3cd58fa2eb899151b534e7941e2207af
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-03-02 23:40:57 +0000

    Deploy martian-lang/martian-rust to github.com/martian-lang/martian-rust.git:gh-pages

--- doc/implementors/core/ops/drop/trait.Drop.js
+++ doc/implementors/core/ops/drop/trait.Drop.js
@@ -3,6 +3,7 @@ implementors["backtrace"] = [{"text":"impl&lt;'_, '_, '_&gt; Drop for BacktraceF
 implementors["csv"] = [{"text":"impl&lt;W:&nbsp;Write&gt; Drop for Writer&lt;W&gt;","synthetic":false,"types":[]}];
 implementors["flate2"] = [{"text":"impl&lt;W:&nbsp;Write&gt; Drop for GzEncoder&lt;W&gt;","synthetic":false,"types":[]}];
 implementors["martian_filetypes"] = [{"text":"impl&lt;T, F, W&gt; Drop for LazyJsonWriter&lt;T, F, W&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;F: MartianFileType + FileStorage&lt;Vec&lt;T&gt;&gt;,<br>&nbsp;&nbsp;&nbsp;&nbsp;W: Write,<br>&nbsp;&nbsp;&nbsp;&nbsp;T: Serialize + DeserializeOwned,&nbsp;</span>","synthetic":false,"types":[]},{"text":"impl&lt;L, T, W&gt; Drop for LazyLz4Writer&lt;L, T, W&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;L: LazyWrite&lt;T, Encoder&lt;W&gt;&gt;,<br>&nbsp;&nbsp;&nbsp;&nbsp;W: Write,&nbsp;</span>","synthetic":false,"types":[]}];
+implementors["once_cell"] = [{"text":"impl&lt;T&gt; Drop for OnceBox&lt;T&gt;","synthetic":false,"types":[]}];
 implementors["regex_syntax"] = [{"text":"impl Drop for Ast","synthetic":false,"types":[]},{"text":"impl Drop for ClassSet","synthetic":false,"types":[]},{"text":"impl Drop for Hir","synthetic":false,"types":[]}];
 implementors["syn"] = [{"text":"impl&lt;'a&gt; Drop for ParseBuffer&lt;'a&gt;","synthetic":false,"types":[]}];
 implementors["tempfile"] = [{"text":"impl Drop for TempDir","synthetic":false,"types":[]},{"text":"impl Drop for TempPath","synthetic":false,"types":[]}];
