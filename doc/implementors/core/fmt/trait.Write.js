1a8f97f8ce62307f51a2157f0c382b64a47ebd50
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-09-09 19:33:04 +0000

    Deploy martian-lang/martian-rust to github.com/martian-lang/martian-rust.git:gh-pages

--- doc/implementors/core/fmt/trait.Write.js
+++ doc/implementors/core/fmt/trait.Write.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["strfmt"] = [{"text":"impl&lt;'a, 'b&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Write.html\" title=\"trait core::fmt::Write\">Write</a> for <a class=\"struct\" href=\"strfmt/struct.Formatter.html\" title=\"struct strfmt::Formatter\">Formatter</a>&lt;'a, 'b&gt;","synthetic":false,"types":["strfmt::formatter::Formatter"]}];
+implementors["strfmt"] = [{"text":"impl&lt;'a, 'b&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.54.0/core/fmt/trait.Write.html\" title=\"trait core::fmt::Write\">Write</a> for <a class=\"struct\" href=\"strfmt/struct.Formatter.html\" title=\"struct strfmt::Formatter\">Formatter</a>&lt;'a, 'b&gt;","synthetic":false,"types":["strfmt::formatter::Formatter"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
