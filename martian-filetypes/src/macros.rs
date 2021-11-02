1c781e6cae06d472589bb6c9acd1094c3bdd5ab2
Adam Azarchs <adam.azarchs@10xgenomics.com>
2021-11-02 16:07:53 -0700

    Mixed tabs and spaces are the devil's work.

--- martian-filetypes/src/macros.rs
+++ martian-filetypes/src/macros.rs
@@ -2,63 +2,63 @@
 #[macro_export]
 macro_rules! martian_filetype_inner {
     ($(#[$attr:meta])* pub struct $name:ident, $extension:expr) => (
-    	$(#[$attr])*
-    	#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
-		// The following attribute ensures that the struct will serialize into a
-		// String like a PathBuf would.
-		#[serde(transparent)]
-		pub struct $name<F>
-		where
-		    F: ::martian::MartianFileType,
-		{
-		    // Skip [de]serializing the inner
-		    #[serde(skip)]
-		    inner: ::std::marker::PhantomData<F>,
-		    path: ::std::path::PathBuf,
-		}
+        $(#[$attr])*
+        #[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+        // The following attribute ensures that the struct will serialize into a
+        // String like a PathBuf would.
+        #[serde(transparent)]
+        pub struct $name<F>
+        where
+            F: ::martian::MartianFileType,
+        {
+            // Skip [de]serializing the inner
+            #[serde(skip)]
+            inner: ::std::marker::PhantomData<F>,
+            path: ::std::path::PathBuf,
+        }
 
         impl<F> ::martian::MartianFileType for $name<F>
-		where
-		    F: ::martian::MartianFileType,
-		{
+        where
+            F: ::martian::MartianFileType,
+        {
             fn extension() -> String {
                 if F::extension().ends_with($extension) || $extension == "" {
-                	F::extension()
+                    F::extension()
                 } else {
-                	format!("{}.{}", F::extension(), $extension)
+                    format!("{}.{}", F::extension(), $extension)
                 }
             }
 
             fn new(file_path: impl AsRef<std::path::Path>, file_name: impl AsRef<std::path::Path>) -> Self {
-				let path = ::martian::utils::make_path(file_path.as_ref(), file_name.as_ref(), Self::extension());
+                let path = ::martian::utils::make_path(file_path.as_ref(), file_name.as_ref(), Self::extension());
                 $name {
-		            inner: ::std::marker::PhantomData,
-		            path,
-		        }
+                    inner: ::std::marker::PhantomData,
+                    path,
+                }
             }
         }
         impl<F> AsRef<std::path::Path> for $name<F>
         where
-        	F: ::martian::MartianFileType
+            F: ::martian::MartianFileType
         {
             fn as_ref(&self) -> &std::path::Path {
                 self.path.as_ref()
             }
         }
 
-    	impl<F, P> From<P> for $name<F>
-		where
-		    ::std::path::PathBuf: From<P>,
-		    F: ::martian::MartianFileType,
-		{
-		    fn from(source: P) -> Self {
-		        let path_buf = ::std::path::PathBuf::from(source);
-		        let file_name = path_buf.file_name().unwrap();
-		        match path_buf.parent() {
-		            Some(path) => ::martian::MartianFileType::new(path, file_name),
-		            None => ::martian::MartianFileType::new("", file_name),
-		        }
-		    }
-		}
+        impl<F, P> From<P> for $name<F>
+        where
+            ::std::path::PathBuf: From<P>,
+            F: ::martian::MartianFileType,
+        {
+            fn from(source: P) -> Self {
+                let path_buf = ::std::path::PathBuf::from(source);
+                let file_name = path_buf.file_name().unwrap();
+                match path_buf.parent() {
+                    Some(path) => ::martian::MartianFileType::new(path, file_name),
+                    None => ::martian::MartianFileType::new("", file_name),
+                }
+            }
+        }
     )
 }
