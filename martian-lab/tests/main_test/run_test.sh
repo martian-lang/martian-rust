ec7623c146acf93e33725e132039366016effb68
Brian Gianforcaro <brian.gianforcaro@10xgenomics.com>
2023-01-05 13:43:10 -0800

    Tests: Allow martian-lab tests to run without needing user path modification
    The script can also be run from any path now.

--- martian-lab/tests/main_test/run_test.sh
+++ martian-lab/tests/main_test/run_test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 MROPATH=$PWD
 MROFLAGS="--disable-ui"
-PATH=$(realpath ../../../target/debug/examples/):$PATH
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+PATH=$(realpath ${SCRIPT_DIR}/../../../deps/bin/):$PATH
+PATH=$(realpath ${SCRIPT_DIR}/../../../target/debug/examples/):$PATH
 mrp pipeline.mro pipeline_test --jobmode=local --disable-ui
