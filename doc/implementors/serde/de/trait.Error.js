498156be25a7775e635b0433bdebcf3093d0826f
adam-azarchs <adam.azarchs@10xgenomics.com>
2023-02-03 18:31:57 +0000

    Deploying to gh-pages from @ martian-lang/martian-rust@b0b0479b37ffdac129f941c83f736d1723705c17 🚀

--- doc/implementors/serde/de/trait.Error.js
+++ doc/implementors/serde/de/trait.Error.js
@@ -1,7 +1,7 @@
-(function() {var implementors = {};
-implementors["bincode"] = [{"text":"impl <a class=\"trait\" href=\"serde/de/trait.Error.html\" title=\"trait serde::de::Error\">Error</a> for <a class=\"type\" href=\"bincode/type.Error.html\" title=\"type bincode::Error\">Error</a>","synthetic":false,"types":["bincode::error::Error"]}];
-implementors["csv"] = [{"text":"impl <a class=\"trait\" href=\"serde/de/trait.Error.html\" title=\"trait serde::de::Error\">Error</a> for <a class=\"struct\" href=\"csv/struct.DeserializeError.html\" title=\"struct csv::DeserializeError\">DeserializeError</a>","synthetic":false,"types":["csv::deserializer::DeserializeError"]}];
-implementors["docopt"] = [{"text":"impl <a class=\"trait\" href=\"serde/de/trait.Error.html\" title=\"trait serde::de::Error\">Error</a> for <a class=\"enum\" href=\"docopt/enum.Error.html\" title=\"enum docopt::Error\">Error</a>","synthetic":false,"types":["docopt::dopt::Error"]}];
-implementors["serde"] = [];
-implementors["serde_json"] = [{"text":"impl <a class=\"trait\" href=\"serde/de/trait.Error.html\" title=\"trait serde::de::Error\">Error</a> for <a class=\"struct\" href=\"serde_json/struct.Error.html\" title=\"struct serde_json::Error\">Error</a>","synthetic":false,"types":["serde_json::error::Error"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {var implementors = {
+"bincode":[["impl <a class=\"trait\" href=\"serde/de/trait.Error.html\" title=\"trait serde::de::Error\">Error</a> for <a class=\"type\" href=\"bincode/type.Error.html\" title=\"type bincode::Error\">Error</a>"]],
+"csv":[["impl <a class=\"trait\" href=\"serde/de/trait.Error.html\" title=\"trait serde::de::Error\">Error</a> for <a class=\"struct\" href=\"csv/struct.DeserializeError.html\" title=\"struct csv::DeserializeError\">DeserializeError</a>"]],
+"docopt":[["impl <a class=\"trait\" href=\"serde/de/trait.Error.html\" title=\"trait serde::de::Error\">Error</a> for <a class=\"enum\" href=\"docopt/enum.Error.html\" title=\"enum docopt::Error\">Error</a>"]],
+"serde":[],
+"serde_json":[["impl <a class=\"trait\" href=\"serde/de/trait.Error.html\" title=\"trait serde::de::Error\">Error</a> for <a class=\"struct\" href=\"serde_json/struct.Error.html\" title=\"struct serde_json::Error\">Error</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
