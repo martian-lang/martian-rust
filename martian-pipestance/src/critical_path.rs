22c2c3452d675ead789261bef11a2becbad3ea5d
Sreenath Krishnan <sreenath.krishnan@10xgenomics.com>
2023-09-14 09:49:46 -0700

    use petgraph

--- martian-pipestance/src/critical_path.rs
+++ martian-pipestance/src/critical_path.rs
@@ -3,9 +3,11 @@
 //!
 
 use crate::common::NodeType;
-use crate::final_state::{ArgumentMode, BindingInfo, FinalState, State};
+use crate::final_state::{ArgumentMode, FinalState};
 use crate::perf::Perf;
 use ordered_float::OrderedFloat;
+use petgraph::algo::toposort;
+use petgraph::graph::{DiGraph, NodeIndex};
 use serde_json::Value;
 use std::collections::{BTreeMap, BTreeSet, VecDeque};
 use std::fmt;
@@ -86,6 +88,9 @@ fn collect_all_nested_strings(v: &Value) -> Vec<String> {
 }
 
 impl CriticalPathBuilder {
+    const START_NODE: &str = "__START__";
+    const END_NODE: &str = "__END__";
+
     fn new(final_state: &FinalState, perf: &Perf) -> Self {
         let return_path_map: BTreeMap<_, _> = final_state
             .completed_stages()
@@ -142,78 +147,85 @@ impl CriticalPathBuilder {
             .or_insert_with(|| StageInfo::new(id.to_string()))
     }
 
-    fn critical_path(mut self) -> CriticalPath {
-        const START: &str = "__START__";
-        const END: &str = "__END__";
+    fn add_start_and_end_node(&mut self) {
         let all_nodes: Vec<_> = self.stage_info.keys().cloned().collect();
 
-        self.mut_stage(START);
-        self.mut_stage(END);
+        self.mut_stage(Self::START_NODE);
+        self.mut_stage(Self::END_NODE);
 
         for node in all_nodes {
-            self.add_link(&node, START);
-            self.add_link(END, &node);
+            self.add_link(&node, Self::START_NODE);
+            self.add_link(Self::END_NODE, &node);
+        }
+    }
+
+    fn critical_path(mut self) -> CriticalPath {
+        self.add_start_and_end_node();
+
+        // Create a directed graph
+        let mut graph = DiGraph::new();
+        let mut node_of_id = BTreeMap::new();
+
+        let ordered_nodes: Vec<_> = self.stage_info.keys().collect();
+
+        for (node, info) in &self.stage_info {
+            let gnode = graph.add_node(OrderedFloat(info.no_queue_wall_time));
+            node_of_id.insert(node, gnode);
         }
 
-        #[derive(Default, Debug)]
-        struct CriticalNodeInfo {
-            no_queue_wall_time: f64,
-            total_time: f64,
-            child: Option<StageId>,
-        }
-
-        let mut critical_info = BTreeMap::<&str, CriticalNodeInfo>::new();
-
-        let mut queue = VecDeque::new();
-        queue.push_back(END);
-
-        while !queue.is_empty() {
-            let node = queue.pop_front().unwrap();
-            let node_info = &self.stage_info[node];
-            for parent in &node_info.parents {
-                queue.push_back(parent);
+        for (node, info) in &self.stage_info {
+            for child in &info.children {
+                graph.update_edge(node_of_id[node], node_of_id[child], ());
             }
-            let max_child = node_info
-                .children
-                .iter()
-                .map(|c| {
-                    (
-                        OrderedFloat(
-                            critical_info
-                                .get(c.as_str())
-                                .map_or(0.0, |info| info.total_time),
-                        ),
-                        c,
-                    )
-                })
-                .max();
-            critical_info.insert(
-                node,
-                CriticalNodeInfo {
-                    no_queue_wall_time: node_info.no_queue_wall_time,
-                    total_time: node_info.no_queue_wall_time + max_child.map_or(0.0, |f| f.0 .0),
-                    child: max_child.map(|f| f.1.clone()),
-                },
-            );
         }
 
-        let mut current_node = START;
-        let mut critical_path = Vec::new();
-        while critical_info[current_node].child.is_some() {
-            current_node = critical_info[current_node].child.as_ref().unwrap();
-            let node_info = &critical_info[current_node];
-            if current_node != END {
-                critical_path.push(CriticalPathNode {
-                    id: current_node.to_string(),
-                    name: current_node.split('.').last().unwrap().to_string(),
-                    no_queue_wall_time_seconds: node_info.no_queue_wall_time,
+        // Find topological order
+        let topological_order: Vec<NodeIndex> = toposort(&graph, None).unwrap();
+
+        #[derive(Default, Debug, PartialEq, Eq, PartialOrd, Ord)]
+        struct MaxChild {
+            weight: OrderedFloat<f64>,
+            child: Option<NodeIndex>,
+        }
+
+        // Calculate maximum weighted path
+        let mut max_children: BTreeMap<NodeIndex, MaxChild> = BTreeMap::new();
+
+        for node in topological_order.iter().rev() {
+            let node_weight = graph[*node];
+            let path_edge = graph
+                .neighbors_directed(*node, petgraph::Direction::Outgoing)
+                .map(|child| MaxChild {
+                    weight: max_children[&child].weight + node_weight,
+                    child: Some(child),
+                })
+                .max()
+                .unwrap_or(MaxChild {
+                    weight: node_weight,
+                    child: None,
+                });
+
+            max_children.insert(*node, path_edge);
+        }
+
+        // Trace back the path
+        let mut path = Vec::new();
+        let mut current_node = topological_order[0];
+        while let Some(child) = max_children[&current_node].child {
+            current_node = child;
+            let node_id = ordered_nodes[current_node.index()];
+            if node_id != CriticalPathBuilder::END_NODE {
+                path.push(CriticalPathNode {
+                    id: node_id.clone(),
+                    name: node_id.split('.').last().unwrap().to_string(),
+                    no_queue_wall_time_seconds: graph[current_node].0,
                 });
             }
         }
 
         CriticalPath {
-            total_time_seconds: critical_info[START].total_time,
-            path: critical_path,
+            total_time_seconds: max_children[&topological_order[0]].weight.0,
+            path,
         }
     }
 }
@@ -225,16 +237,50 @@ mod tests {
     use crate::PipestanceFile;
     use anyhow::Result;
 
-    #[test]
-    fn test_critical_path() -> Result<()> {
+    fn path_builder() -> Result<CriticalPathBuilder> {
         let final_state = FinalState::from_string(read_zst("test_data/_finalstate.zst")?)?;
         let perf = Perf::from_string(read_zst("test_data/_perf.zst")?)?;
-
         let builder = CriticalPathBuilder::new(&final_state, &perf);
-        println!("{:#?}", builder.stage_info);
+        Ok(builder)
+    }
+
+    #[test]
+    fn test_critical_path() -> Result<()> {
+        let builder = path_builder()?;
+        // println!("{:#?}", builder.stage_info);
 
         let critical_path = builder.critical_path();
         println!("{:#?}", critical_path);
+
+        let max_weight = critical_path.total_time_seconds;
+        let stages: Vec<_> = critical_path.path.into_iter().map(|p| p.name).collect();
+        assert!((max_weight - 3263.950373765).abs() / max_weight <= 1e-6);
+        assert_eq!(
+            stages,
+            [
+                "WRITE_GENE_INDEX",
+                "PARSE_TARGET_FEATURES",
+                "MAKE_SHARD",
+                "MAKE_HD_CORRECTION_MAP",
+                "BARCODE_CORRECTION",
+                "SET_ALIGNER_SUBSAMPLE_RATE",
+                "ALIGN_AND_COUNT",
+                "WRITE_H5_MATRIX",
+                "FILTER_BARCODES",
+                "COLLATE_PROBE_METRICS",
+                "WRITE_MOLECULE_INFO",
+                "DISABLE_SECONDARY_ANALYSIS",
+                "ANALYZER_PREFLIGHT",
+                "PREPROCESS_MATRIX",
+                "RUN_PCA",
+                "RUN_GRAPH_CLUSTERING",
+                "COMBINE_CLUSTERING",
+                "RUN_DIFFERENTIAL_EXPRESSION",
+                "SUMMARIZE_ANALYSIS",
+                "DECONVOLVE_SPOTS",
+                "CLOUPE_PREPROCESS",
+            ]
+        );
         Ok(())
     }
 }
