3f70d1adea9c11ca7f3c04c39fc4761e2184be6e
Preyas Shah <preyas.shah@10xgenomics.com>
2024-07-22 12:00:55 -0700

    fix: skip deserialization, just use a &str (#519)
    * skip deserialization, just use a &str
    
    * do Copy again

--- martian/src/stage.rs
+++ martian/src/stage.rs
@@ -117,7 +117,7 @@ impl<T: MartianFileType> MartianMakePath for T {
 ///
 /// Memory/ thread request can be negative in matrian. See
 /// [http://martian-lang.org/advanced-features/#resource-consumption](http://martian-lang.org/advanced-features/#resource-consumption)
-#[derive(Debug, Serialize, Deserialize, Clone, Default)]
+#[derive(Debug, Serialize, Deserialize, Copy, Clone, Default)]
 pub struct Resource {
     #[serde(rename = "__mem_gb", skip_serializing_if = "Option::is_none")]
     mem_gb: Option<isize>,
@@ -125,8 +125,12 @@ pub struct Resource {
     threads: Option<isize>,
     #[serde(rename = "__vmem_gb", skip_serializing_if = "Option::is_none")]
     vmem_gb: Option<isize>,
-    #[serde(rename = "__special", skip_serializing_if = "Option::is_none")]
-    special: Option<String>,
+    #[serde(
+        rename = "__special",
+        skip_serializing_if = "Option::is_none",
+        skip_deserializing
+    )]
+    special: Option<&'static str>,
 }
 
 impl Resource {
@@ -167,8 +171,8 @@ impl Resource {
     }
 
     /// Get the special resource request
-    pub fn get_special(&self) -> Option<String> {
-        self.special.clone()
+    pub fn get_special(&self) -> Option<&'static str> {
+        self.special
     }
 
     /// Set the mem_gb
@@ -217,13 +221,13 @@ impl Resource {
     /// ```rust
     /// use martian::Resource;
     ///
-    /// let resource = Resource::new().mem_gb(2).vmem_gb(4).special("gpu_count1_mem8".to_owned());
+    /// let resource = Resource::new().mem_gb(2).vmem_gb(4).special("gpu_count1_mem8");
     /// assert_eq!(resource.get_mem_gb(), Some(2));
     /// assert_eq!(resource.get_vmem_gb(), Some(4));
     /// assert_eq!(resource.get_threads(), None);
-    /// assert_eq!(resource.get_special(), Some("gpu_count1_mem8".to_owned()));
+    /// assert_eq!(resource.get_special(), Some("gpu_count1_mem8"));
     /// ```
-    pub fn special(mut self, special: String) -> Self {
+    pub fn special(mut self, special: &'static str) -> Self {
         self.special = Some(special);
         self
     }
@@ -633,7 +637,7 @@ pub trait MartianStage: MroMaker {
                     fill_defaults(resource),
                 ))
             }
-            let rover = _chunk_prelude(chunk_idx, run_directory, chunk.resource.clone())?;
+            let rover = _chunk_prelude(chunk_idx, run_directory, chunk.resource)?;
             self.main(args.clone(), chunk.inputs.clone(), rover)
         };
 
