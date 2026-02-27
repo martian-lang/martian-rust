93f1de2900c3519c1890bd2980ef26b7a76ddfda
macklin-10x <chris.macklin@10xgenomics.com>
2026-02-27 23:38:03 +0000

    Deploying to gh-pages from @ martian-lang/martian-rust@4f80f548f8fbc92e3a85cb6e449b773bd41fb3b6 🚀

---
+++ doc/trait.impl/serde_core/ser/trait.Error.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["bincode",[["impl <a class=\"trait\" href=\"serde_core/ser/trait.Error.html\" title=\"trait serde_core::ser::Error\">Error</a> for <a class=\"type\" href=\"bincode/type.Error.html\" title=\"type bincode::Error\">Error</a>"]]],["csv",[["impl <a class=\"trait\" href=\"serde_core/ser/trait.Error.html\" title=\"trait serde_core::ser::Error\">Error</a> for <a class=\"struct\" href=\"csv/struct.Error.html\" title=\"struct csv::Error\">Error</a>"]]],["serde",[]],["serde_core",[]],["serde_json",[["impl <a class=\"trait\" href=\"serde_core/ser/trait.Error.html\" title=\"trait serde_core::ser::Error\">Error</a> for <a class=\"struct\" href=\"serde_json/struct.Error.html\" title=\"struct serde_json::Error\">Error</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[226,221,13,18,242]}
